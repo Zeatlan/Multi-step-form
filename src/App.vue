b943e09df7e5e4e2bcec904ab14f5f9bb886c855
Zeatlan <salimalexo@gmail.com>
2023-05-08 21:21:40 +0200

    feat: nav design

---
+++ src/App.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="box-form">
+    <div class="box-form__progress">
+
+      <div class="task" v-for="(task, key) in tasks" :key="key">
+        <div class="task__number">{{ key+1 }}</div>
+        <div class="task__text">
+          <span>STEP {{ key+1 }}</span>
+          <p>{{ task }}</p>
+        </div>
+      </div>
+
+      <NavDecoration />
+
+    </div>
+    <div class="box-form__content">Lorem ipsum content</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import NavDecoration from './components/NavDecoration.vue';
+
+const tasks = ['Your info', 'Select plan', 'Add-ons', 'Summary'];
+</script>
+
+<style lang="scss">
+html, body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-color: color(neutral, magnolia);
+  height: 100%;
+}
+
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  font-family: $font-family;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: $base-font-size;
+}
+
+.box-form {
+  width: 65%;
+  display: flex;
+  background-color: color(neutral, white);
+  border-radius: 15px;
+  padding: 2rem;
+  gap: 2rem;
+
+  &__progress {
+    position: relative;
+    flex: 1;
+    background: color(primary, purplish-blue);
+    display: flex;
+    flex-direction: column;
+    padding: 2rem;
+    border-radius: 15px;
+    gap: 2rem;
+    overflow: hidden;
+
+    .task {
+      display: flex;
+      align-items: flex-start;
+      gap: 1rem;
+
+      &__number {
+        border: 1px solid color(neutral, white);
+        color: color(neutral, white);
+        padding: 0.4em;
+        border-radius: 50%;
+        width: 1.5em;
+        height: 1.5em;
+        line-height: 1.5em;
+        text-align: center;
+      }
+
+      &__text {
+        span {
+          color: color(primary, pastel-blue);
+          font-size: 12px;
+        }
+
+        p {
+          margin: 3px 0 0 0;
+          color: color(neutral, magnolia);
+          text-transform: uppercase;
+          font-weight: 600;
+        }
+      }
+    }
+  }
+
+  &__content {
+    flex: 3;
+    height: 550px;
+  }
+}
+</style>
---
+++ src/components/NavDecoration.vue
@@ -0,0 +1,77 @@
+<template>
+      <svg viewBox="0 0 160 100" class="blue-curve">
+        <path d="M 0 0 C 100 0 -50 180 500 60 L 0 500 L 0 0" fill="hsl(243, 100%, 72%)" opacity="0.8"></path>
+      </svg>
+
+      <div class="confettis">
+        <div class="springle springle-left"></div>
+        <div class="springle springle-center"></div>
+        <div class="springle springle-right"></div>
+      </div>
+
+      <div class="yellow-circle"></div>
+      
+       <svg id="svg-blob" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" class="red-blob">
+        <path fill="#ED667FFF" d="M25.5,-30.5C28.3,-22.8,22.4,-11.4,20.9,-1.5C19.4,8.4,22.2,16.8,19.5,22.4C16.8,28.1,8.4,31,-0.2,31.2C-8.8,31.5,-17.6,29,-24.8,23.3C-32,17.6,-37.5,8.8,-35.1,2.4C-32.7,-4,-22.3,-7.9,-15.1,-15.6C-7.9,-23.2,-4,-34.6,3.7,-38.3C11.4,-42,22.8,-38.1,25.5,-30.5Z" width="100%" height="100%" transform="translate(50 50)" stroke-width="0" style="transition: all 0.3s ease 0s;"></path>
+      </svg>
+</template>
+
+<style scoped lang="scss">
+
+
+    .red-blob {
+      position: absolute;
+      right: -150px;
+      bottom: -150px;
+      width: 350px;
+    }
+
+    .blue-curve {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+    }
+
+    .yellow-circle {
+      position: absolute;
+      bottom: 15px;
+      left: -70px;
+      height: 135px;
+      width: 135px;
+      background-color: #ffaf7e;
+      border-radius: 50%;
+    }
+
+    .confettis {
+      position: absolute;
+      bottom: 70px;
+      right: 50px;
+      width: 50px;
+      z-index: 2;
+
+      .springle {
+        position: absolute;
+        background: color(neutral, white);
+        width: 20px;
+        height: 5px;
+        border-radius: 50px;
+
+        &-left {
+          transform: rotate(-45deg);
+          left: -15px;
+          bottom: 35px;
+        }
+
+        &-center {
+          transform: rotate(110deg);
+          bottom: 0;
+        }
+
+        &-right {
+          transform: rotate(35deg);
+          bottom: 25px;
+          right: 10px;
+        }
+      }
+    }
+  </style>
